0ac15d33a5456215f1ae5c0507fddea3cc32ca12
Mridul Gupta <mridul1940.be22@chitkara.edu.in>
2025-02-16 10:15:31 +0530

    user details

---
+++ Server/Models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    UserName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    Email:  {
+        type: String,
+        required: true,
+        unique: true
+    },
+    Age: {
+        type: Number,
+        required: true
+    },
+    Gender: {
+        type: String,
+        required: true
+    },
+    Password: {
+        type: String,
+        required: true
+    }
+})
+module.exports = mongoose.model('UserDetails', userSchema);
--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,27 @@
+require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+
 const app = express();
 const port = 5000;
+mongoose.connect(process.env.MongoUri).then(()=>{
+    console.log("MongoDB Connected");
+}).catch((err)=>{
+    console.log(err);
+})
+
+const {SignUpFunction,LoginFunction} = require('./Endpoints/signup_login')
+
+app.use(express.json())
+app.use(cors())
+
+app.post('/signup', SignUpFunction);
+app.post('/login', LoginFunction);
+
+// app.post('')
+
+
 app.listen(port , ()=>{
     console.log("Server is Runing.... ");
 })
