8bc124eeba8878fd81f6b3155093ad33e6949e39
Sumanyu301 <sumanyukodan301@gmail.com>
2025-02-21 15:25:38 +0530

    added signup and signin authentication using zod

--- Server/Endpoints/signup_login.js
+++ Server/Endpoints/signup_login.js
@@ -1,10 +1,20 @@
 const UserDetails = require("../Models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const signUpSchema = require("../Middlewares/zodAuthenticatio").signUpSchema;
+const loginSchema = require("../Middlewares/zodAuthenticatio").loginSchema;
+
 const secret_key = "wercvbn";
 
 const SignUpFunction = function (req, res) {
-  // console.log(req.body);
+  // Validate request body
+  const validation = signUpSchema.safeParse(req.body);
+  if (!validation.success) {
+    return res
+      .status(400)
+      .json({ msg: "Invalid input", errors: validation.error.errors });
+  }
+
   const Crypted_Password = bcrypt.hashSync(req.body.password, 8);
   const User = new UserDetails({
     Email: req.body.email,
@@ -14,17 +24,18 @@ const SignUpFunction = function (req, res) {
     Password: Crypted_Password,
     Weight: req.body.weight,
   });
-  const isUser = UserDetails.findOne({ Email: req.body.Email });
+
+  const isUser = UserDetails.findOne({ Email: req.body.email });
   isUser.then((user) => {
     if (user) {
       res.status(200).json({
-        msg: "Already a account ",
+        msg: "Already an account",
       });
     } else {
       User.save().then((user) => {
         const token = jwt.sign({ UserId: user._id }, secret_key);
         return res.status(201).json({
-          msg: "Signup Successfull",
+          msg: "Signup Successful",
           token: token,
         });
       });
@@ -33,19 +44,25 @@ const SignUpFunction = function (req, res) {
 };
 
 const LoginFunction = function (req, res) {
+  // Validate request body
+  const validation = loginSchema.safeParse(req.body);
+  if (!validation.success) {
+    return res
+      .status(400)
+      .json({ msg: "Invalid input", errors: validation.error.errors });
+  }
+
   const isUser = UserDetails.findOne({ Email: req.body.Email });
   isUser.then((user) => {
     if (!user) {
       res.status(201).json({
-        msg: "Recheck Your Email ID ",
+        msg: "Recheck Your Email ID",
       });
     } else {
       if (bcrypt.compareSync(req.body.Password, user.Password)) {
-        const token = jwt.sign({ UserId: user._id }, secret_key, {
-          expiresIn: "1h",
-        });
+        const token = jwt.sign({ UserId: user._id }, secret_key);
         return res.status(202).json({
-          msg: "Login Successfull",
+          msg: "Login Successful",
           token: token,
         });
       } else {
---
+++ Server/Middlewares/zodAuthenticatio.js
@@ -0,0 +1,19 @@
+const { z } = require("zod");
+
+// Define Zod schemas for validation
+const signUpSchema = z.object({
+  email: z.string().email(),
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+  age: z.number().int().positive(),
+  gender: z.string().min(1),
+  password: z.string().min(6),
+  weight: z.number().positive(),
+});
+
+const loginSchema = z.object({
+  Email: z.string().email(),
+  Password: z.string().min(6),
+});
+
+module.exports = { loginSchema, signUpSchema };
