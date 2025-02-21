6d45d77d46eb7ddc2727bc9063aa103d100fd124
Sumanyu301 <sumanyukodan301@gmail.com>
2025-02-21 16:16:59 +0530

    Added weight and gender in signin and sigup response

--- Server/Endpoints/signup_login.js
+++ Server/Endpoints/signup_login.js
@@ -37,6 +37,8 @@ const SignUpFunction = function (req, res) {
         return res.status(201).json({
           msg: "Signup Successful",
           token: token,
+          gender: user.Gender,
+          weight: user.Weight,
         });
       });
     }
@@ -52,26 +54,35 @@ const LoginFunction = function (req, res) {
       .json({ msg: "Invalid input", errors: validation.error.errors });
   }
 
-  const isUser = UserDetails.findOne({ Email: req.body.Email });
-  isUser.then((user) => {
-    if (!user) {
-      res.status(201).json({
-        msg: "Recheck Your Email ID",
-      });
-    } else {
-      if (bcrypt.compareSync(req.body.Password, user.Password)) {
+  const isUser = UserDetails.findOne({ Email: req.body.email });
+  isUser
+    .then((user) => {
+      if (!user) {
+        return res.status(201).json({
+          msg: "Recheck Your Email ID",
+        });
+      }
+
+      if (bcrypt.compareSync(req.body.password, user.password)) {
         const token = jwt.sign({ UserId: user._id }, secret_key);
         return res.status(202).json({
           msg: "Login Successful",
           token: token,
+          gender: user.Gender, // Changed from isUser.Gender
+          weight: user.Weight, 
         });
       } else {
-        res.status(400).json({
+        return res.status(400).json({
           msg: "Incorrect Password...",
         });
       }
-    }
-  });
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        msg: "Server error",
+        error: error.message,
+      });
+    });
 };
 
 module.exports = { SignUpFunction, LoginFunction };
